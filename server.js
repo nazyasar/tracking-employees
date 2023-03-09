436675c90e4c4baae29bf75512c0b43d0e54e42a
Naz Yasar <nazyasar95@gmail.com>
2023-03-08 22:22:25 -0500

    db connection added

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
-
-
-
-
-
-
+//database connection
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -13,5 +16,21 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'istanbul34',
     database: 'employee_db'
-})
+},
+console.log(`Connected to the database.`)
+);
+
+// Query database
+connection.query('SELECT * FROM employee', function (err, results) {
+    console.log(results);
+  });
+  
+  // Default response for any other request (Not Found)
+  app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 
