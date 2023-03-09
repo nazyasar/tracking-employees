f012d8b3d30bfde45ffe30ed2a76b5bf8070f93f
Naz Yasar <nazyasar95@gmail.com>
2023-03-08 20:53:03 -0500

    server.js created

---
+++ server.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'istanbul34',
+    database: 'employee_db'
+})
+
